cf39b860a7edd37f6cc19572c2ebbf7f1da637da
Laiismartins26 <93133593+Laiismartins26@users.noreply.github.com>
2025-05-12 10:30:43 -0300

    Add files via upload

---
+++ olá.php
@@ -0,0 +1,21 @@
+<h1>Minha primeira aula de php</h1>
+
+<?php
+// Exibindo uma mensagem na tela
+echo "Olá, mundo!";
+echo "<br>"; // quebra de linha
+
+$nome = "Laís"; //spring
+$idade = 17; //inteiro
+$altura = 1.70; //float
+$peso = 60; //float
+$casado = false; //booleano
+$filhos = null; //nulo - sem valor definido
+
+echo "Nome : $nome <br>";
+echo "Idade: $idade <br>";
+echo "Altura: $altura <br>";
+echo "peso: $peso <br>";
+echo "casado: $casado <br>";
+echo "filhos: $filhos <br>";
+?>
