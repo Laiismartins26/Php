da870109f7a54950f6c2d563ad4edbd2b5b92bdf
Laiismartins26 <93133593+Laiismartins26@users.noreply.github.com>
2025-05-12 12:03:22 -0300

    Add files via upload

---
+++ imc.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html Lang="pt-BR">
+<head> 
+    <meta charset= "UTF-8">
+    <title>IMC Simples</title>
+</head> 
+<body> 
+    <h2>Calculadora de IMC</h2>
+
+    <form method="post">
+        Peso(kg): <input type= "number" name="peso" 
+        step="0.1" required><br><br>
+        Altura (m): <input type= "number" name="altura" 
+        step="0.01" required><br><br>
+        <input type= "submit" value="Calcular IMC">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST) {
+    $peso = $_POST["peso"];
+    $altura = $_POST["altura"];
+
+    if ($altura > 0) {
+     $imc = $peso / ($altura * $altura);
+     echo "Seu IMC é: <strong> . number_format
+     (num: $imc, decimals: 2) . "</strong></p>";
+
+     if ($imc < 18.5) {
+        echo "<p>Classificação: Abaixo do peso</
+        p>"
+     } elseif ($imc < 25) {
+        echo "<p> Classificação: Peso normal </p>";
+     } elseif ($imc < 29.9){
+        echo "<p> Classificação: Sobrepeso";
+     } else {
+        echo "Você está obeso";
+     }
+     }
+</body>
+</html>
